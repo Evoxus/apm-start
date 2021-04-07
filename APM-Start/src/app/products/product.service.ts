937fef693e052b52d018f6c73c43b2141a275095
Michael Ploughman <michael.ploughman@blndspt.com>
2021-04-07 15:39:08 -0600

    Setting up "http" requests

--- APM-Start/src/app/products/product.service.ts
+++ APM-Start/src/app/products/product.service.ts
@@ -1,33 +1,17 @@
+import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
 import { IProducts } from "./products";
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
+  private productUrl = 'api/products/products.json'
 
-  getProducts(): IProducts[] {
-    return [
-      {
-        "productId": 2,
-        "productName": "Garden Cart",
-        "productCode": "GDN-0023",
-        "releaseDate": "March 18, 2021",
-        "description": "15 gallon capacity rolling cart",
-        "price": 32.99,
-        "starRating": 4.2,
-        "imageUrl": "assets/images/garden_cart.png"
-      },
-      {
-        "productId": 5,
-        "productName": "Hammer",
-        "productCode": "TBX-0048",
-        "releaseDate": "May 21, 2021",
-        "description": "Curved claw steel hammer",
-        "price": 8.9,
-        "starRating": 4.8,
-        "imageUrl": "assets/images/hammer.png"
-      }
-    ];
+  constructor(private http: HttpClient) { }
+
+  getProducts(): Observable<IProducts[]> {
+    return this.http.get<IProducts[]>(this.productUrl);
   }
 }
