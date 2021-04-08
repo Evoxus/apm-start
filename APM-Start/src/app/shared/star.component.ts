4f688ebd9118ac7d2a89ac4bbbdb1cfcc5e5bd34
Michael Ploughman <michael.ploughman@blndspt.com>
2021-04-08 14:56:23 -0600

    Product Guards

--- APM-Start/src/app/shared/star.component.ts
+++ APM-Start/src/app/shared/star.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, Output, OnChanges, EventEmitter } from "@angular/core";
+import { Component, Input, OnChanges } from "@angular/core";
 
 
 @Component({
@@ -9,14 +9,10 @@ import { Component, Input, Output, OnChanges, EventEmitter } from "@angular/core
 export class StarComponent implements OnChanges {
   @Input() rating: number = 0;
   cropWidth: number = 75;
-  @Output() ratingClicked: EventEmitter<string> = 
-    new EventEmitter<string>();
+
   
   ngOnChanges(): void {
     this.cropWidth = this.rating * 75/5;
   }
 
-  onClick(): void{
-    this.ratingClicked.emit(`The rating ${this.rating} was clicked!`)
-  }
 }
